56462f588851d54854206d642238f72161e4429c
kisy34 <konilson777@gmail.com>
2023-09-09 14:22:08 +0900

    s2

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,5 @@
 from django.urls import include, path
 from rest_framework.routers import SimpleRouter
-
 from views import (FollowToView, FollowView, IngredientViewSet, RecipeViewSet,
                    TagViewSet, UserViewSet)
 
